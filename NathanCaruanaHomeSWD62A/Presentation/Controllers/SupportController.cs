5fb4811a3c855568294281c54e5e3c8654dcf6e7
nathanc1 <49673028+nathanc1@users.noreply.github.com>
2020-12-15 20:48:42 +0100

    Creating the basis for my clean architecture

--- NathanCaruanaHomeSWD62A/Presentation/Controllers/SupportController.cs
+++ NathanCaruanaHomeSWD62A/Presentation/Controllers/SupportController.cs
@@ -12,5 +12,21 @@ namespace Presentation.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Contact(string email, string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                ViewData["warning"] = "Question was left empty";
+
+            }
+            else
+            {
+                ViewData["feedback"] = "Thank you for your query, we will get back to you asp";
+            }
+            //ViewBag.feedback = "";
+            return View();
+        }
     }
 }

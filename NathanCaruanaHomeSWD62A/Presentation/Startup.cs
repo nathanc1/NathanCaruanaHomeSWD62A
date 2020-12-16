2d60c1c496c6ac0a8cb66c800f6d365c43c95dd7
nathanc1 <49673028+nathanc1@users.noreply.github.com>
2020-12-16 22:08:13 +0100

    Dependency Injection

--- NathanCaruanaHomeSWD62A/Presentation/Startup.cs
+++ NathanCaruanaHomeSWD62A/Presentation/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ShoppingCart.Data.Context;
+using ShoppingCart.IOC;
 
 namespace Presentation
 {
@@ -40,6 +41,8 @@ namespace Presentation
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ NathanCaruanaHomeSWD62A/ShoppingCart.IOC/DependencyContainer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShoppingCart.Application.Interfaces;
+using ShoppingCart.Application.Services;
+using ShoppingCart.Data.Repositories;
+using ShoppingCart.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.IOC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //when are the instances trigerred??
+            //as soon as the application starts?
+            //as soon as the user makes the first call?
+            //as soon as the user makes the second call? and so on and forth..
+            //...
+
+            //answer: https://www.tutorialsteacher.com/core/dependency-injection-in-aspnet-core
+
+            /*Singleton: IoC container will create and share a single instance of a service throughout the application's lifetime.
+            Transient: The IoC container will create a new instance of the specified service type every time you ask for it.
+            Scoped: IoC container will create an instance of the specified service type once per request and will be shared in a single request.
+            */
+
+
+
+            services.AddScoped<IProductsRepository, ProductsRepository>();
+            services.AddScoped<IProductsService, ProductsService>();
+        }
+    }
+}

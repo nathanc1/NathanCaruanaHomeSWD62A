22b838383c93212c71d73e70dd5d2e4b595d0880
nathanc1 <49673028+nathanc1@users.noreply.github.com>
2020-12-16 19:31:10 +0100

    First Migration to Db

---
+++ NathanCaruanaHomeSWD62A/ShoppingCart.Data/Context/ShoppingCartDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCart.Data.Context
+{
+    public class ShoppingCartDBContext : DbContext
+    {
+        public ShoppingCartDBContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Category> Category { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+        }
+    }
+}
---
+++ NathanCaruanaHomeSWD62A/ShoppingCart.Domain/Model/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ShoppingCart.Domain.Model
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+        
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+    }
+}
